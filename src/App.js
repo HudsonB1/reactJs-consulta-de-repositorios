7941cda64c01f151f56ae36f707a6276b4db5a05
Hudson Baroni <hudsonbarni@gmail.com>
2022-05-19 10:53:30 -0300

    Consumindo API do GitHub com axio

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App(props) {
   const [ usuario, setUsuario] = useState('');
+  
+  function handlePesquisa() {
+      axios.get(`https://api.github.com/users/${usuario}/repos`).then(response => console.log(response.data));
+  }
+  
   return (
       <header className="App-header">
-        <h1 className='teamo'>TE AMO GATA</h1>
-        <input onChange={e => setUsuario(e.target.value)} className="pesquisa" placeholder="Usuário"/>
-        <button className="button" type="buttun">Pesquisar</button>
+        <h1 className='teamo'>Hello, World !</h1>
+        <input onChange={e => setUsuario(e.target.value)} value={usuario} className="pesquisa" placeholder="Usuário"/>
+        <button className="button" type="buttun" onClick={handlePesquisa} >Pesquisar</button>
       </header>
   );
 }
